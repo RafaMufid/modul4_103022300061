f2c5efbfc47a555640681f960fe6c183a4feadde
RafaMufid <akurafamufid@gmail.com>
2025-03-13 10:45:01 +0700

    menambahkan class KodeProduk

--- modul4_103022300061/Program.cs
+++ modul4_103022300061/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class KodeProduk
+{
+    public static string getKodeProduk(string produk)
+    {
+        string[,] dataProduk =
+        {
+            {"Laptop", "E100"},
+            {"Smartphone", "E101"},
+            {"Tablet", "E102"},
+            {"Headset", "E103"},
+            {"Keyboard", "E104"},
+            {"Mouse", "E105"},
+            {"Printer", "E106"},
+            {"Monitor", "E107"},
+            {"Smartwatch", "E108"},
+            {"Kamera", "E109"}
+        };
+        
+        for (int i = 0; i < dataProduk.GetLength(0); i++)
+        {
+            if (dataProduk[i, 0].Equals(produk, StringComparison.OrdinalIgnoreCase))
+            {
+                return dataProduk[i, 1];
+            }
+        }
+        return "Produk tidak ditemukan";
+    }
+}
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Masukkan nama produk elektronik: ");
+        string produkElektronik = Console.ReadLine() ?? "";
+
+        string kodeProduk = KodeProduk.getKodeProduk(produkElektronik);
+        Console.WriteLine($"Kode Produk {produkElektronik} : {kodeProduk}");
+    }
+}
